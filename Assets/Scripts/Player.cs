ebeab8481bee621cc54f234743bfe0f3393a1f40
Jason Lu <JasonLu2027@u.northwestern.edu>
2026-08-01 00:11:48 -0500

    progress: coyotetime

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,24 +9,31 @@ public class Player : MonoBehaviour
     private Transform _t;
     private Rigidbody2D _rb;
     private Keyboard _k;
+    private playerState _state;
+    
+    //this is to keep track of currently active coyoteTimes;
+    private Coroutine _coyoteTime;
+    private bool _hasJumped;
+    
     
     //terminal velocity downwards
     [Tooltip("terminal velocity")]
     public float maxFallingSpeed;
+
+    [Tooltip("Amount of time that a jump input can be received after leaving solid ground")]
+    public float coyoteTime;
     
     public float walkingSpeed;
     public float jumpSpeed;
-    
-    
-    
-    
     
     
     void Start()
     {
         _t = transform;
         _k = Keyboard.current;
-        _rb=GetComponent<Rigidbody2D>();
+        _rb = GetComponent<Rigidbody2D>();
+        _coyoteTime = null;
+        _hasJumped = true;
     }
 
     // Update is called once per frame
@@ -35,10 +42,6 @@ public class Player : MonoBehaviour
         //Input.GetKey is not really working in Unity's new input system
         
         //go left when press left
-        if (_k.aKey.wasPressedThisFrame)
-        {
-            print("akey");
-        }
         if (_k.aKey.isPressed)
         {
             _t.position += walkingSpeed * Time.deltaTime * Vector3.left;
@@ -50,13 +53,66 @@ public class Player : MonoBehaviour
             _t.position += walkingSpeed * Time.deltaTime * Vector3.right;
         }
         
-        //as of now, Jump whenever press space
-        if (_k.spaceKey.isPressed)
+        
+        if (_k.spaceKey.wasPressedThisFrame && 
+            !_hasJumped &&
+            (
+                _state == playerState.grounded ||
+                _coyoteTime != null
+                )
+            )
         {
             //with some other condition, permit the jump
             _rb.linearVelocity = new Vector2(0,jumpSpeed);
+            _hasJumped = true;
         }
         
         
+        
+    }
+    
+    void OnCollisionEnter2D(Collision2D collision){
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            _state = playerState.grounded;
+            _hasJumped = false;
+            CeaseIfActive(ref _coyoteTime);
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            _state = playerState.airborne;
+            CeaseIfActive(ref _coyoteTime);
+            _coyoteTime = StartCoroutine(CoyoteTime());
+        }
+    }
+
+    void CeaseIfActive(ref Coroutine c)
+    {
+        if (c != null)
+        {
+            StopCoroutine(c);
+            c = null;
+        }
+    }
+
+    //coyote timer
+    //build in delay
+    IEnumerator CoyoteTime()
+    {
+        yield return new WaitForSeconds(coyoteTime);
+        _coyoteTime = null;
+    }
+
+
+    enum playerState
+    {
+        grounded,
+        airborne,
+        phasing
     }
 }
