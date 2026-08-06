4c91d0d92388081061fe999865b6d512f3e0c0ad
Jason Lu <JasonLu2027@u.northwestern.edu>
2026-08-06 07:20:02 -0500

    PIXELS BABY

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -17,6 +17,7 @@ public class CameraMovement : MonoBehaviour
     void Start()
     {
         p = Player.p;
+        Screen.SetResolution(640, 360, false);
     }
 
     // Update is called once per frame
